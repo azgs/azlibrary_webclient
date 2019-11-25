be4f308ed402ace03ee3e2df2eb50723a9f02484
Douglas Meredith <douglasm@email.arizona.edu>
2019-11-25 16:56:48 -0700

    Set up basic error page

--- app.js
+++ app.js
@@ -57,8 +57,11 @@ app.use(morgan('dev', {
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+//app.engine('html', require('ejs').renderFile);
+//app.set('view engine', 'html');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
 const routes = require('./app.routes');
 
